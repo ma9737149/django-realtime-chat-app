4f13bb6c281a0ab98ab207ab4d20b495d5604d02
= <ahmedashraf3695@gmail.com>
2025-07-03 18:34:31 +0200

    doing some changes here

--- chat/models.py
+++ chat/models.py
@@ -1,19 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.files.base import ContentFile
-from cryptography.fernet import Fernet
+import hashlib
+import os
 
-key = Fernet.generate_key()
-cipher = Fernet(key)
 
-class EncryptedImageField(models.ImageField):
-    def save(self, name, content, save=True):
-        image_data = content.read()  
-        encrypted_data = cipher.encrypt(image_data) 
-        
-        encrypted_content = ContentFile(encrypted_data)
-        
-        super().save(name, encrypted_content, save)
+def get_file_hash(file):
+    hasher = hashlib.sha256()
+    file.seek(0)
+    for chunk in file.chunks():
+        hasher.update(chunk)
+    file.seek(0)
+    return hasher.hexdigest()
+
+class HashedImageField(models.ImageField):
+    def generate_filename(self, instance, filename):
+        file = instance.__dict__.get(self.attname)
+        if file:
+            file_hash = get_file_hash(file.file)
+            ext = os.path.splitext(filename)[1].lower()
+            new_filename = f"{file_hash}{ext}"
+            return os.path.join(self.upload_to, new_filename)
+        return super().generate_filename(instance, filename)
+
+    def save_form_data(self, instance, data):
+        super().save_form_data(instance, data)
 
 class UserProfile(AbstractUser):
-    profile = EncryptedImageField(upload_to='users/' , default = 'defaults/default-img.png')
+    profile = HashedImageField(upload_to='users/' , default = 'defaults/default-img.png')
